fd334afe76a36fd464e84e56993202a4bf4bfaf7
soryrawyer <sory.rawyer@gmail.com>
2018-05-31 13:39:30 -0400

    add sha1 hash to issue body as a means to discern if we've already seen this TODO; add a TODO label to collect all of our TODO issues

--- riposte.py
+++ riposte.py
@@ -3,11 +3,12 @@ riposte.py : turn TODOS into issues
 """
 
 import base64
+import hashlib
 import os
 import re
 
 from flask import Flask, request
-from github import Github
+from github import Github, GithubException
 
 app = Flask(__name__)
 
@@ -22,9 +23,16 @@ def payload():
     """
     commit_fields = ['added', 'modified']
     files_to_search = set()
+    issue_shas = set()
     request_json = request.get_json()
     commits = request_json['commits']
     repo = GIT_CLIENT.get_repo(request_json['repository']['full_name'])
+    try:
+        label = repo.get_label('todos')
+    except GithubException:
+        label = repo.create_label('todos', '006b75')
+    for issue in repo.get_issues(labels=[label]):
+        issue_shas.add(issue.body)
     for commit in commits:
         # for every commit, add filenames for new and changed files
         for field in commit_fields:
@@ -34,18 +42,21 @@ def payload():
         # get the contents of each file
         # search file for todo comments
         # create issue based on comments
-        todo_statement = get_todo_from_file(repo, filename)
-        if todo_statement:
-            repo.create_issue(todo_statement)
+        todo_statements = get_todo_from_file(repo, filename)
+        if todo_statements:
+            for statement in todo_statements:
+                sha1 = hashlib.sha1()
+                sha1.update(statement)
+                comment_hash = sha1.hexdigest()
+                if comment_hash in issue_shas:
+                    continue
+                repo.create_issue(statement.decode('utf-8'), body=comment_hash, labels=[label])
     return 'get your post request'
 
-def get_todo_from_file(repo, filename: str) -> str:
+def get_todo_from_file(repo, filename: str) -> list:
     """
     get_todo_from_file : get contents of file from github
     search contents for a todo statement in a comment
     """
     contents = base64.b64decode(repo.get_contents(filename).content)
-    re_result = re.search('# TODO: (.*)', str(contents))
-    if re_result:
-        return [group[0] for group in re_result.groups()]
-    return None
+    return re.findall(b'# TODO: (.*)', contents)
